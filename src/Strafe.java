4d0c42c7e00bc5235f189fe337116bd1dc2e5829
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-16 18:07:52 +0100

    Übungsbeispiel 5

--- src/Strafe.java
+++ src/Strafe.java
@@ -8,4 +8,78 @@ public class Strafe {
     int anzahl; //zählt die Anzahl der Vergehen
 
     //Erhöt das Attribut strafe aufgrund einer Geschwindigkeitsüberschreitung. Erhöhen Sie die Anzahl der Vergehen
+    public void strafe(int geschwindigkeitsueberschreitung){
+        if (geschwindigkeitsueberschreitung < 20){
+            strafe += 30;
+            anzahl++;
+            System.out.println("Geschwindigkeistüberschreitung von: " + geschwindigkeitsueberschreitung + " es fallen 30 Euro an und die Anzahl der Vergehen ist jetzt: " + anzahl + " hoch.");
+        }else if (geschwindigkeitsueberschreitung < 30){
+            strafe += 50;
+            anzahl++;
+            System.out.println("Geschwindigkeistüberschreitung von: " + geschwindigkeitsueberschreitung + " es fallen 50 Euro an und die Anzahl der Vergehen ist jetzt: " + anzahl + " hoch.");
+        }else if (geschwindigkeitsueberschreitung < 50){
+            strafe += 100;
+            anzahl++;
+            System.out.println("Geschwindigkeistüberschreitung von: " + geschwindigkeitsueberschreitung + " es fallen 100 Euro an und die Anzahl der Vergehen ist jetzt: " + anzahl + " hoch.");
+
+        }else if (geschwindigkeitsueberschreitung < 100){
+            strafe += 500;
+            anzahl++;
+            System.out.println("Geschwindigkeistüberschreitung von: " + geschwindigkeitsueberschreitung + " es fallen 500 Euro an und die Anzahl der Vergehen ist jetzt: " + anzahl + " hoch.");
+
+        }else {
+            strafe += 1500;
+            anzahl++;
+            System.out.println("Geschwindigkeistüberschreitung von: " + geschwindigkeitsueberschreitung + " es fallen 1500 Euro an und die Anzahl der Vergehen ist jetzt: " + anzahl + " hoch.");
+
+        }
+    }
+
+    //Erhöhen Sie die Strafe aufgrund eines vergessenen Verbandspaketes um 25 Euro. Erhöhen Sie die Anzahl der Vergehen.
+    public void verbandspaket(){
+        strafe += 20;
+        anzahl++;
+        System.out.println("Da das Verbandspaket fehlt fallen 20 Euro an, die Strafe beträgt: " + strafe + " Euro und die Anzahl der Vergehen beträgt jetzt: " +anzahl);
+    }
+
+    //Erhöhen Sie die Strafe aufgrund eines erhöhten Alkoholspiegels. Erhöhen Sie die Anzahl der Vergehen um Zwei.
+    public void alkohol(double wert) {
+        if (wert < 1.0) {
+            strafe += 100;
+            anzahl += 2;
+            System.out.println("Aufgrund eines Alkoholspiegels von " + wert + " Promille wird die Strafe um 100 € erhöht und beträgt jetzt " + strafe + " Euro und die Anzahl der Vergehen wird um 2 erhöht und beträgt nun: " + anzahl);
+        } else if (wert < 2.0) {
+            strafe += 400;
+            anzahl += 2;
+            System.out.println("Aufgrund eines Alkoholspiegels von " + wert + " Promille wird die Strafe um 400 € erhöht und beträgt jetzt " + strafe + " Euro und die Anzahl der Vergehen wird um 2 erhöht und beträgt nun: " + anzahl);
+        } else if (wert < 3.0) {
+            strafe += 1000;
+            anzahl += 2;
+            System.out.println("Aufgrund eines Alkoholspiegels von " + wert + " Promille wird die Strafe um 1000 € erhöht und beträgt jetzt " + strafe + " Euro und die Anzahl der Vergehen wird um 2 erhöht und beträgt nun: " + anzahl);
+        } else{
+            strafe += 5000;
+            anzahl += 2;
+            System.out.println("Aufgrund eines Alkoholspiegels von " + wert + " Promille wird die Strafe um 5000 € erhöht und beträgt jetzt " + strafe + " Euro und die Anzahl der Vergehen wird um 2 erhöht und beträgt nun: " + anzahl);
+        }
+    }
+
+    //Erhöhen Sie die Strafe um den übergebenen Wert. Erhöhen Sie die Anzahl der Vergehen.
+    public void sonstiges(double wert){
+        strafe += wert;
+        anzahl++;
+        System.out.println("Durch ein sonstiges vergehen wird die Strafe um " + wert + " Euro erhöht und beträgt jetzt gesamt " + strafe + " Euro. Die Anzahl der vergehen ist: " + anzahl);
+    }
+
+    //Schreiben Sie eine Methode für das Auslesen der Strafe. Aufgrund folgender Regelung bekommt der Verkehrssünder jedoch eine Verringerung gutgeschrieben:
+
+    public double getStrafe(int anzahl){
+        if (anzahl == 1){
+            strafe = strafe - (strafe * 0.3);
+        }else if (anzahl == 2){
+            strafe = strafe - (strafe * 0.2);
+        }else if (anzahl == 3){
+            strafe = strafe - (strafe * 0.1);
+        }
+        return strafe;
+    }
 }
--- src/StrafeDemo.java
+++ src/StrafeDemo.java
@@ -1,5 +1,30 @@
 public class StrafeDemo {
     public static void main(String[] args) {
+        Strafe miri = new Strafe();
+        miri.vorname = "Mirjam";
+        miri.nachname = "Weirer";
+        miri.kennzeichen = "G-897UM";
+        miri.strafnnummer = 2;
+        miri.strafe = 0;
+        miri.anzahl = 0;
 
+        Strafe tommy = new Strafe();
+        tommy.vorname = "Thomas";
+        tommy.nachname = "Mölecnik";
+        tommy.kennzeichen = "LE-454HE";
+        tommy.strafnnummer = 33;
+        tommy.strafe = 0;
+        tommy.anzahl = 2;
+
+        System.out.println(miri.nachname + " " + miri.vorname + ", " + miri.kennzeichen + " Kennzeichen " + miri.strafnnummer + " Strafnummer:");
+        miri.strafe(15);
+        miri.sonstiges(15.5);
+        System.out.println("Die Strafe bträgt nun (nach möglichem Rabatt) " + miri.getStrafe(miri.anzahl));
+        System.out.println();
+        System.out.println(tommy.nachname + " " + tommy.vorname + " " + tommy.kennzeichen + " Kennzeichen " + tommy.strafnnummer + " Strafnummer:");
+        tommy.strafe(40);
+        tommy.verbandspaket();
+        tommy.alkohol(1.0);
+        System.out.println("Die Strafe bträgt nun (nach möglichem Rabatt) " + miri.getStrafe(tommy.anzahl));
     }
 }
