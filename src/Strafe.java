8a89457b6f0a836f30cd735e7d4de02e4fa56648
MirjamWeirer <mirjam.weirer@gmx.at>
2021-03-16 17:04:50 +0100

    Übungsbeispiel 5

---
+++ src/Strafe.java
@@ -0,0 +1,11 @@
+public class Strafe {
+    //Attrinute
+    String vorname; //Vorname
+    String nachname; //Nachname
+    String kennzeichen; //PKW-Kennzeichen
+    int strafnnummer; //Fortlaufende Nummer
+    double strafe; //Höhe der Strafe
+    int anzahl; //zählt die Anzahl der Vergehen
+
+    //Erhöt das Attribut strafe aufgrund einer Geschwindigkeitsüberschreitung. Erhöhen Sie die Anzahl der Vergehen
+}
---
+++ src/StrafeDemo.java
@@ -0,0 +1,5 @@
+public class StrafeDemo {
+    public static void main(String[] args) {
+
+    }
+}
